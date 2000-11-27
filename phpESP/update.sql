22dd47adcc363bfda3ae68012218e7584058f62f
jimmerman <jimmerman@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2000-11-27 15:50:05 +0000

    update to add column to answers_other for choice_id
    git-svn-id: svn://svn.code.sf.net/p/phpesp/code/trunk@37 b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe

--- phpESP/update.sql
+++ phpESP/update.sql
@@ -1,9 +1,9 @@
 -- # $Id$
 -- #
--- # Upgrade script to make database changes between 
--- # release-1.0 and release-1.1. Please read the 
+-- # Upgrade script to make database changes between
+-- # release-1.0 and release-1.1. Please read the
 -- # UPGRADE file before attempting to upgrade.
--- # 
+-- #
 -- # Written by James Flemer
 -- # <jflemer@acm.jhu.edu>
 -- # <jflemer@egrad2000.com>
@@ -20,3 +20,6 @@ INSERT INTO question_types VALUES ('8','Rate (scale 1..5)',1,'answers_rank');
 
 UPDATE result_types SET name='Ordered' WHERE id='2';
 INSERT INTO result_types VALUES ('5','Average Rank');
+
+-- # UPGRADE FROM v1.1 to v1.2beta1
+ALTER TABLE answers_other ADD COLUMN choice_id INT NOT NULL;
