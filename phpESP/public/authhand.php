717a6a4f2228a778f1b649466dc4222701bc905e
jimmerman <jimmerman@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2001-04-07 20:05:30 +0000

    o Merged changes in from 'xml' branch.   (This branch would have been called something else,    had I realized I was going to do so many things --    none of which actually got to XML yet.) o This will likely break a lot. :-)
    git-svn-id: svn://svn.code.sf.net/p/phpesp/code/trunk@134 b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe

--- phpESP/public/authhand.php
+++ phpESP/public/authhand.php
@@ -2,105 +2,120 @@
 
 # $Id$
 
-// Written by James Flemer <jflemer@acm.jhu.edu>
-//        and Romans Jasins <roma@latnet.lv>
+// Written by James Flemer
+// For eGrad2000.com
+// <jflemer@acm.jhu.edu>
+// <jflemer@eGrad2000.com>
 
-	require("/usr/home/jflemer/phpESP/admin/phpESP.ini");
-	require($PIECES."/funcs".$EXT);
+#	global $HTTP_SERVER_VARS, $HTTP_POST_VARS, $HTTP_GET_VARS;
+	require($HTTP_SERVER_VARS['DOCUMENT_ROOT']. "/phpESP/admin/phpESP.ini");
+	require($ESPCONFIG['include_path']."/funcs".$ESPCONFIG['extension']);
 
-	if(empty($sid)) {
-		echo(mkerror('Error processing survey: Survey not specified.'));
+	if(isset($HTTP_GET_VARS['sid'])) {
+		echo(mkerror(_('Error processing survey: Security violation.')));
 		return;
 	}
 	
+	if(isset($HTTP_GET_VARS['results']) || isset($HTTP_POST_VARS['results'])) {
+		echo(mkerror(_('Error processing survey: Security violation.')));
+		return;
+	}
+	
+	$sid = intval($sid);
+	if(empty($sid))
+		$sid = intval($HTTP_POST_VARS['sid']);
+
+	if(empty($sid)) {
+		echo(mkerror(_('Error processing survey: Survey not specified.')));
+		return;
+	}
+
 	if(empty($HTTP_POST_VARS['userid'])) {
 		// find remote user id (takes the first non-empty of the folowing)
 		//  1. a GET variable named 'userid'
-		//  2. the PHP_AUTH_USER
-		//  3. the REMOTE_USER set by HTTP-Authintication
-		//  4. the query string
+		//  2. the REMOTE_USER set by HTTP-Authintication
+		//  3. the query string
 		if (!empty($HTTP_GET_VARS['userid'])) {
-			$userid = $HTTP_GET_VARS['userid'];
-		} elseif(!empty($PHP_AUTH_USER)) {
-			$userid = $PHP_AUTH_USER;
-		} elseif(!empty($REMOTE_USER)) {
-			$userid = $REMOTE_USER;
-		} elseif(!empty($QUERY_STRING)) {
-			$userid = urldecode($QUERY_STRING);
+			$HTTP_POST_VARS['userid'] = $HTTP_GET_VARS['userid'];
+		} elseif(!empty($HTTP_SERVER_VARS['REMOTE_USER'])) {
+			$HTTP_POST_VARS['userid'] = $HTTP_SERVER_VARS['REMOTE_USER'];
+		} elseif(!empty($HTTP_SERVER_VARS['QUERY_STRING'])) {
+			$HTTP_POST_VARS['userid'] = urldecode($HTTP_SERVER_VARS['QUERY_STRING']);
+		} else {
+			$HTTP_POST_VARS['userid'] = 'unknown';
 		}
-		$HTTP_POST_VARS['userid'] = $userid;
 	}
 
 	if(empty($HTTP_POST_VARS['referer']))
-		$referer = $HTTP_REFERER;
+		$HTTP_POST_VARS['referer'] = $HTTP_SERVER_VARS['HTTP_REFERER'];
 
-	if(empty($HTTP_POST_VARS['sec'])) {
+	if(empty($HTTP_POST_VARS['sec']) || $HTTP_POST_VARS['sec'] < 1) {
+		$HTTP_POST_VARS['sec'] = 1;
+
 		// check for authorization on the survey
 		include($PIECES."/auth".$EXT);
 		if(!survey_auth($sid,$PHP_AUTH_USER,$PHP_AUTH_PW))
 			return;
 	}
 
+	if(empty($HTTP_POST_VARS['rid']))
+		$HTTP_POST_VARS['rid'] = '';
+
 	// show results instead of show survey
 	// but do not allow getting results from URL or FORM
-	if($results && empty($HTTP_GET_VARS['results']) && empty($HTTP_POST_VARS['results'])) {
-		survey_results($sid);
+	if($results) {
+		// small security issue here, anyone could pick a QID to crossanalyze
+		survey_results($sid,$precision,$totals,$qid,$cids);
 		return;
 	}
 
 	// else draw the survey
-	$sql = "SELECT status FROM surveys WHERE id='${sid}'";
+	$sql = "SELECT status FROM survey WHERE id='${sid}'";
 	$result = mysql_query($sql);
-	$status = mysql_result($result,0,0);
+	$status = @mysql_result($result,0,0);
 	if($status & ( STATUS_DONE | STATUS_DELETED )) {
-		echo(mkerror('Error processing survey: Survey is not active.'));
+		echo(mkerror(_('Error processing survey: Survey is not active.')));
 		return;
 	}
 	if(!($status & STATUS_ACTIVE)) {
 		if(!($test && ($status & STATUS_TEST))) {
-			echo(mkerror('Error processing survey: Survey is not active.'));
+			echo(mkerror(_('Error processing survey: Survey is not active.')));
 			return;
 		}
 	}
 
-	$num_sections = count_sections($sid);
-
-	if(empty($HTTP_POST_VARS['sec']) || $HTTP_POST_VARS['sec'] < 1)
-		$sec = 1;
+	$num_sections = survey_num_sections($sid);
 
 	$msg = '';
 
-	if(!empty($submit)) {
-		$msg = check_required($sid,$sec);
+	if(!empty($HTTP_POST_VARS['submit'])) {
+		$msg = response_check_required($sid,$HTTP_POST_VARS['sec']);
 		if(empty($msg)) {
-			$rid = insert_values($sid,$sec,$rid);
-			complete_response($rid);
-			email_values($sid,$rid);
-			goto_thankyou($sid,$referer);
-			$rid = '';
+			$HTTP_POST_VARS['rid'] = response_insert($sid,$HTTP_POST_VARS['sec'],$HTTP_POST_VARS['rid']);
+			response_commit($HTTP_POST_VARS['rid']);
+			response_send_email($sid,$HTTP_POST_VARS['rid']);
+			goto_thankyou($sid,$HTTP_POST_VARS['referer']);
 			return;
 		}
 	}
 
 	if(!empty($HTTP_POST_VARS['next'])) {
-		$msg = check_required($sid,$sec);
+		$msg = response_check_required($sid,$HTTP_POST_VARS['sec']);
 		if(empty($msg)) {
-			$rid = insert_values($sid,$sec,$rid);
-			$sec++;
+			$rid = response_insert($sid,$HTTP_POST_VARS['sec'],$HTTP_POST_VARS['rid']);
+			$HTTP_POST_VARS['sec']++;
 		}
 	}
 ?>
-<form method="post" action="<?php echo($PHP_SELF); ?>">
-<input type="hidden" name="referer" value="<?php echo($referer); ?>">
-<input type="hidden" name="userid" value="<?php echo($userid); ?>">
+<form method="post" action="<?php echo($HTTP_SERVER_VARS['PHP_SELF']); ?>">
+<input type="hidden" name="referer" value="<?php echo($HTTP_POST_VARS['referer']); ?>">
+<input type="hidden" name="userid" value="<?php echo($HTTP_POST_VARS['userid']); ?>">
 <input type="hidden" name="sid" value="<?php echo($sid); ?>">
-<input type="hidden" name="rid" value="<?php echo($rid); ?>">
-<input type="hidden" name="sec" value="<?php echo($sec); ?>">
-
-
-<?php	render_survey($sid,$sec,$msg); ?>
-<?php	if($sec == $num_sections)	{ ?>
-	<input type="submit" name="submit" value="Finish">
+<input type="hidden" name="rid" value="<?php echo($HTTP_POST_VARS['rid']); ?>">
+<input type="hidden" name="sec" value="<?php echo($HTTP_POST_VARS['sec']); ?>">
+<?php	render_survey($sid,$HTTP_POST_VARS['sec'],$msg); ?>
+<?php	if($HTTP_POST_VARS['sec'] == $num_sections)	{ ?>
+	<input type="submit" name="submit" value="Submit Survey">
 <?php	} else { ?>
 	<input type="submit" name="next" value="Next Page">
 <?php	} ?>
