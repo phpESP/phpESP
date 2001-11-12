7b4956219b9e1e2bf6da1e2bf6141cad01de19ac
jimmerman <jimmerman@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2001-11-12 23:50:05 +0000

    o New database spec.
    git-svn-id: svn://svn.code.sf.net/p/phpesp/code/trunk@188 b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe

--- phpESP/phpESP.sql
+++ phpESP/phpESP.sql
@@ -4,33 +4,76 @@
 -- # Written by James Flemer
 -- # For eGrad2000.com
 -- # <jflemer@acm.jhu.edu>
--- # <jflemer@egrad2000.com>
--- #
 
--- # types of questions
-CREATE TABLE question_type (
-	id				INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	type			CHAR(64) NOT NULL,
-	has_choices		ENUM('Y','N') NOT NULL,
-	response_table	CHAR(64) NOT NULL,
-	PRIMARY KEY (id)
+-- ...............................................................
+-- ....................... USERS/GROUPS ..........................
+-- ...............................................................
+
+-- # realm (group) table
+CREATE TABLE realm ( 
+	name		CHAR(16) NOT NULL,
+	title		CHAR(64) NOT NULL,
+	changed		TIMESTAMP(14) NOT NULL DEFAULT '',
+	PRIMARY KEY(name)
 );
 
--- # types of results
-CREATE TABLE result_type (
-	id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	type		CHAR(64) NOT NULL,
-	PRIMARY KEY (id)
+-- # table of respondents (people who enter data / take surveys)
+CREATE TABLE respondent ( 
+	username	CHAR(16) NOT NULL,
+	password	CHAR(16) NOT NULL,
+	auth		CHAR(16) NOT NULL DEFAULT 'BASIC',
+	realm		CHAR(16) NOT NULL,
+	fname		CHAR(16),
+	lname		CHAR(24),
+	email		CHAR(64),
+	disabled	ENUM('Y','N') NOT NULL DEFAULT 'N',
+	changed		TIMESTAMP(14) NOT NULL DEFAULT '',
+	expiration	TIMESTAMP(14),
+	PRIMARY KEY (username, realm)
 );
+
+-- # table of designers (people who create forms / surveys)
+CREATE TABLE designer (
+	username	CHAR(16) NOT NULL,
+	password	CHAR(16) NOT NULL,
+	auth		CHAR(16) NOT NULL DEFAULT 'BASIC',
+	realm		CHAR(16) NOT NULL,
+	fname		CHAR(16),
+	lname		CHAR(24),
+	email		CHAR(64),
+	pdesign		ENUM('Y','N') NOT NULL DEFAULT 'Y',
+	pstatus		ENUM('Y','N') NOT NULL DEFAULT 'N',
+	pdata		ENUM('Y','N') NOT NULL DEFAULT 'N',
+	pall		ENUM('Y','N') NOT NULL DEFAULT 'N',
+	pgroup		ENUM('Y','N') NOT NULL DEFAULT 'N',
+	puser		ENUM('Y','N') NOT NULL DEFAULT 'N',
+	disabled	ENUM('Y','N') NOT NULL DEFAULT 'N',
+	changed		TIMESTAMP(14) NOT NULL DEFAULT '',
+	expiration	TIMESTAMP(14),
+	PRIMARY KEY(username, realm)
+);
+
+-- # create the _special_ superuser group
+-- # members of this group have superuser status
+INSERT INTO realm ( name, title )
+	VALUES ( 'superuser', 'ESP System Administrators' );
+
+-- # default root account
+INSERT INTO designer (username, password, fname, lname, realm, pdesign, pstatus, pdata, pall, pgroup, puser, disabled)
+	VALUES ('root', PASSWORD('esp'), 'ESP', 'Superuser', 'superuser', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'N');
+
+-- ...............................................................
+-- ..................... SURVEYS/FORMS ...........................
+-- ...............................................................
 
 -- # table of different surveys available
 CREATE TABLE survey (
 	id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	created		TIMESTAMP(14) NOT NULL DEFAULT '',
-	owner		CHAR(64),
-	public		ENUM('Y','N') NOT NULL DEFAULT 'N',
-	status		INT UNSIGNED NOT NULL DEFAULT '0',
 	name		CHAR(64) NOT NULL,
+	owner		CHAR(16) NOT NULL,
+	realm		CHAR(64) NOT NULL,
+	public		ENUM('Y','N') NOT NULL DEFAULT 'Y',
+	status		INT UNSIGNED NOT NULL DEFAULT '0',
 	title		CHAR(255) NOT NULL,
 	email		CHAR(64),
 	subtitle	TEXT,
@@ -39,86 +82,34 @@ CREATE TABLE survey (
 	thanks_page	CHAR(255),
 	thank_head	CHAR(255),
 	thank_body	TEXT,
+	changed		TIMESTAMP(14) NOT NULL DEFAULT '',
 	PRIMARY KEY (id),
 	UNIQUE(name)
 );
 
--- # table of users
-CREATE TABLE user ( 
-	username	CHAR(64) NOT NULL,
-	password	CHAR(64) NOT NULL,
-	firstname	CHAR(64),
-	lastname	CHAR(64),
-	email		CHAR(64),
-	realm		CHAR(64),
-	disabled	ENUM('Y','N') NOT NULL DEFAULT 'N',
-	created		TIMESTAMP(14) NOT NULL DEFAULT '',
-	changed		TIMESTAMP(14) NOT NULL DEFAULT '',
-	expiration	TIMESTAMP(14),
-	PRIMARY KEY (username)
+-- # types of questions
+CREATE TABLE question_type (
+	id				INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	type			CHAR(32) NOT NULL,
+	has_choices		ENUM('Y','N') NOT NULL,
+	response_table	CHAR(32) NOT NULL,
+	PRIMARY KEY (id)
 );
-
--- # table of managers
-CREATE TABLE manager (
-	username	CHAR(64) NOT NULL,
-	password	CHAR(64) NOT NULL,
-	firstname	CHAR(64),
-	lastname	CHAR(64),
-	email		CHAR(64),
-	realm		CHAR(64),
-	screate		ENUM('Y','N') NOT NULL DEFAULT 'N',
-	sactivate	ENUM('Y','N') NOT NULL DEFAULT 'N',
-	send		ENUM('Y','N') NOT NULL DEFAULT 'N',
-	sdelete		ENUM('Y','N') NOT NULL DEFAULT 'N',
-	seeall		ENUM('Y','N') NOT NULL DEFAULT 'N',
-	users		ENUM('Y','N') NOT NULL DEFAULT 'N',
-	managers	ENUM('Y','N') NOT NULL DEFAULT 'N',
-	superuser	ENUM('Y','N') NOT NULL DEFAULT 'N',
-	disabled	ENUM('Y','N') NOT NULL DEFAULT 'N',
-	created		TIMESTAMP(14) NOT NULL DEFAULT '',
-	changed		TIMESTAMP(14) NOT NULL DEFAULT '',
-	expiration	TIMESTAMP(14),
-	PRIMARY KEY(username)
-);
-
-INSERT INTO manager (
-		username, password, firstname, lastname,
-		screate, sactivate, send, sdelete, seeall, users, managers,	superuser)
-	VALUES (
-		'root',PASSWORD('esp'),'ESP','Superuser',
-		'Y','Y','Y','Y','Y','Y','Y','Y');
-
--- # access table
-CREATE TABLE access ( 
-	survey_id	INT UNSIGNED NOT NULL,
-	realm		CHAR(64) NOT NULL,
-	restricted	ENUM('Y','N') NOT NULL DEFAULT 'N',
-	maxresponses INT UNSIGNED NOT NULL DEFAULT '0',
-	start_time	TIMESTAMP(14),
-	end_time	TIMESTAMP(14),
-	PRIMARY KEY (survey_id)
-);
-
--- # access table
-CREATE TABLE usergroup ( 
-	survey_id	INT UNSIGNED NOT NULL,
-	sgroup		CHAR(64) NOT NULL,
-	PRIMARY KEY(survey_id,sgroup)
-);
-
 
 -- # table of the questions for all the surveys
 CREATE TABLE question (
 	id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	survey_id	INT UNSIGNED NOT NULL,
+	name		CHAR(30) NOT NULL,
 	type_id		INT UNSIGNED NOT NULL,
 	result_id	INT UNSIGNED,
-	parent_id	INT UNSIGNED,
+	length		INT NOT NULL DEFAULT 0,
+	precise		INT NOT NULL DEFAULT 0,
 	position	INT UNSIGNED NOT NULL,
 	content		TEXT NOT NULL,
 	required	ENUM('Y','N') NOT NULL DEFAULT 'N',
 	deleted		ENUM('Y','N') NOT NULL DEFAULT 'N',
-	private		ENUM('Y','N') NOT NULL DEFAULT 'N',
+	public		ENUM('Y','N') NOT NULL DEFAULT 'Y',
 	PRIMARY KEY (id)
 );
 
@@ -131,7 +122,29 @@ CREATE TABLE question_choice (
 	PRIMARY KEY (id)
 );
 
--- # answer_* SUBMITTED BY USER (below this line)
+-- # access control to adding data to a form / survey
+CREATE TABLE access (
+	id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	survey_id	INT UNSIGNED NOT NULL,
+	realm		CHAR(16),
+	maxlogin	INT UNSIGNED DEFAULT '0',
+	PRIMARY KEY(id)
+);
+
+-- ...............................................................
+-- ..................... RESPONSE DATA ...........................
+-- ...............................................................
+
+-- # this table holds info to distinguish one servey response from another
+-- # (plus timestamp, and username if known)
+CREATE TABLE response (
+	id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	survey_id	INT UNSIGNED NOT NULL,
+	submitted	TIMESTAMP(14) NOT NULL DEFAULT '',
+	complete	ENUM('Y','N') NOT NULL DEFAULT 'N',
+	username	CHAR(16),
+	PRIMARY KEY (id)
+);
 
 -- # answers to boolean questions (yes/no)
 CREATE TABLE response_bool (
@@ -178,40 +191,30 @@ CREATE TABLE response_text (
 
 -- # answers to any Other: ___ questions
 CREATE TABLE response_other (
-	id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	response_id	INT UNSIGNED NOT NULL,
 	question_id INT UNSIGNED NOT NULL,
 	choice_id	INT UNSIGNED NOT NULL,
 	response	TEXT,
-	PRIMARY KEY (id)
+	PRIMARY KEY (response_id, question_id, choice_id)
 );
 
--- # this table holds info to distinguish one servey response from another
--- # (plus timestamp, and username if known)
-CREATE TABLE response (
-	id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	survey_id	INT UNSIGNED NOT NULL,
-	submitted	TIMESTAMP(14) NOT NULL DEFAULT '',
-	complete	ENUM('Y','N') NOT NULL DEFAULT 'N',
-	username	CHAR(64),
-	PRIMARY KEY (id)
+-- # answers to any date questions
+CREATE TABLE response_date (
+	response_id	INT UNSIGNED NOT NULL,
+	question_id INT UNSIGNED NOT NULL,
+	response	DATE,
+	PRIMARY KEY (response_id,question_id)
 );
 
 -- # populate the types of questions
 INSERT INTO question_type VALUES ('1','Yes/No','N','response_bool');
-INSERT INTO question_type VALUES ('2','Single Line Text Entry','N','response_text');
-INSERT INTO question_type VALUES ('3','Essay (Multiline)','N','response_text');
+INSERT INTO question_type VALUES ('2','Text Box','N','response_text');
+INSERT INTO question_type VALUES ('3','Essay Box','N','response_text');
 INSERT INTO question_type VALUES ('4','Radio Buttons','Y','response_single');
 INSERT INTO question_type VALUES ('5','Check Boxes','Y','response_multiple');
 INSERT INTO question_type VALUES ('6','Dropdown Box','Y','response_single');
 -- INSERT INTO question_type VALUES ('7','Rating','N','response_rank');
 INSERT INTO question_type VALUES ('8','Rate (scale 1..5)','Y','response_rank');
+INSERT INTO question_type VALUES ('9','Date','N','response_date');
+INSERT INTO question_type VALUES ('10','Numeric','N','response_text');
 INSERT INTO question_type VALUES ('99','Page Break','N','');
-
-
--- # populate the type of results
-INSERT INTO result_type VALUES ('1','Percentages');
-INSERT INTO result_type VALUES ('2','Ordered');
-INSERT INTO result_type VALUES ('3','Count');
-INSERT INTO result_type VALUES ('4','List');
-INSERT INTO result_type VALUES ('5','Average Rank');
