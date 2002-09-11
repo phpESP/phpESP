9e05e7611d54b9b47f13a6ca519de25f19236b6d
jimmerman <jimmerman@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2002-09-11 05:00:19 +0000

    o Fix warnings in E_ALL. o Fix path to phpESP.ini. o Make css including a bit more sane in auto-handler.

--- public/survey.php
+++ public/survey.php
@@ -4,6 +4,7 @@
 	
 	$_name = '';
 	$_title = '';
+    $_css = '';
 	if (isset($HTTP_GET_VARS['name'])) {
 		$_name = XADDSLASHES($HTTP_GET_VARS['name']);
 		unset($HTTP_GET_VARS['name']);
@@ -11,47 +12,47 @@
 			ereg_replace('(^|&)name=[^&]*&?', '', $HTTP_SERVER_VARS['QUERY_STRING']);
 	}
 
-	//let's also see if the survey that's about to be rendered has an associated theme
 	if (!empty($_name)) {
-        	$sql = "SELECT id,title,theme FROM survey WHERE name = '$_name'";
-        	if ($result = mysql_query($sql)) {
-	        	if (mysql_num_rows($result) > 0)
-		        	list($sid, $_title, $css_file) = mysql_fetch_row($result);
-			mysql_free_result($result);
-		}
+        $_sql = "SELECT id,title,theme FROM survey WHERE name = '$_name'";
+        if ($_result = mysql_query($_sql)) {
+            if (mysql_num_rows($_result) > 0)
+                list($sid, $_title, $_css) = mysql_fetch_row($_result);
+            mysql_free_result($_result);
+        }
+        unset($_sql);
+        unset($_result);
 	}
-	// need to take care of the scenario where a user may submit a survey without completing all
-	// required questions. If this occurs we need to ensure that the survey will still be rendered
-	// using the desired theme (if set).
-	else if (isset($HTTP_SERVER_VARS['HTTP_REFERER'])) {
-        	list ($survey_url, $survey_name) = split('=',$HTTP_SERVER_VARS['HTTP_REFERER']);
-		$sql = "SELECT theme FROM survey WHERE name = '$survey_name'";
-                if ($result = mysql_query($sql)) {
-                       	if (mysql_num_rows($result) > 0){
-                               	list($theme) = mysql_fetch_row($result);
-                        }
-                        mysql_free_result($result);
-                }
-	}
+
+    // call the handler-prefix once $sid is set to handle
+    // authentication / authorization 
 	include($ESPCONFIG['handler_prefix']);
+
+	if (empty($_name) && isset($sid) && $sid) {
+        $_sql = "SELECT title,theme FROM survey WHERE id = '$sid'";
+        if ($_result = mysql_query($_sql)) {
+            if (mysql_num_rows($_result) > 0){
+                list($_title, $_css) = mysql_fetch_row($_result);
+            }
+            mysql_free_result($_result);
+        }
+        unset($_sql);
+        unset($_result);
+	}
+
 ?>
 <html>
 <head><title><?php echo($_title); ?></title>
 <?php
-if (isset($css_file)) {
-	echo("<link rel=\"stylesheet\" href=\"".$GLOBALS['ESPCONFIG']['survey_css_dir'].$css_file."\"  type=\"text/css\">\n");
-}
-else if(isset($theme)) {
-	 echo("<link rel=\"stylesheet\" href=\"".$GLOBALS['ESPCONFIG']['survey_css_dir'].$theme."\"  type=\"text/css\">\n");
-}
+    if (!empty($_css)) {
+	    echo('<link rel="stylesheet" href="'. $GLOBALS['ESPCONFIG']['survey_css_dir'].$_css ."\" type=\"text/css\">\n");
+    }
+    unset($_css);
 ?>
 </head>
 <body>
 <?php
 	unset($_name);
 	unset($_title);
-	unset($css_file);
-	unset($theme);
 	include($ESPCONFIG['handler']);
 ?>
 </body>
