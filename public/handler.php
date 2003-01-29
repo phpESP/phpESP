120fa130546caa418c8a8a2391a55e9b13dfba99
jimmerman <jimmerman@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2003-01-29 17:18:14 +0000

    Added the ability to save surveys for later completion. Added navigation within a multi-page survey.
    Sponsored By: Neon Cowboy Design (http://www.neoncowboy.com/)
    --
    o Move some variable checking into -prefix area.
    o Add checking of new access options: navigate and resume.
    o Use lowercase HTML tags.

--- public/handler.php
+++ public/handler.php
@@ -23,14 +23,6 @@
 		return;
 	}
 
-	if(empty($HTTP_POST_VARS['rid']))
-		$HTTP_POST_VARS['rid'] = '';
-	
-	if (empty($HTTP_POST_VARS['sec']))
-		$HTTP_POST_VARS['sec'] = 1;
-	else
-		$HTTP_POST_VARS['sec'] = intval($HTTP_POST_VARS['sec']);
-
 	if (empty($HTTP_POST_VARS['referer']))
 		$HTTP_POST_VARS['referer'] = '';
 
@@ -43,9 +35,13 @@
 	}
 
 	// else draw the survey
-	$sql = "SELECT status FROM survey WHERE id='${sid}'";
+	$sql = "SELECT status, name FROM survey WHERE id='${sid}'";
 	$result = mysql_query($sql);
-	$status = @mysql_result($result,0,0);
+    if ($result && mysql_num_rows($result) > 0)
+    	list ($status, $name) = mysql_fetch_row($result);
+    else
+        $status = 0;
+
 	if($status & ( STATUS_DONE | STATUS_DELETED )) {
 		echo(mkerror(_('Error processing survey: Survey is not active.')));
 		return;
@@ -61,9 +57,15 @@
 
 	$msg = '';
 
+	$action = 'http://' . $HTTP_SERVER_VARS['HTTP_HOST'] . $HTTP_SERVER_VARS['PHP_SELF'];
+	if (!empty($HTTP_SERVER_VARS['QUERY_STRING']))
+		$action .= "?" . $HTTP_SERVER_VARS['QUERY_STRING'];
+
 	if(!empty($HTTP_POST_VARS['submit'])) {
 		$msg = response_check_required($sid,$HTTP_POST_VARS['sec']);
 		if(empty($msg)) {
+            if ($ESPCONFIG['auth_response'] && auth_get_option('resume'))
+                response_delete($sid, $HTTP_POST_VARS['rid'], $HTTP_POST_VARS['sec']);
 			$HTTP_POST_VARS['rid'] = response_insert($sid,$HTTP_POST_VARS['sec'],$HTTP_POST_VARS['rid']);
 			response_commit($HTTP_POST_VARS['rid']);
 			response_send_email($sid,$HTTP_POST_VARS['rid']);
@@ -72,25 +74,45 @@
 		}
 	}
 
+	if(!empty($HTTP_POST_VARS['resume']) && $ESPCONFIG['auth_response'] && auth_get_option('resume')) {
+        response_delete($sid, $HTTP_POST_VARS['rid'], $HTTP_POST_VARS['sec']);
+		$HTTP_POST_VARS['rid'] = response_insert($sid,$HTTP_POST_VARS['sec'],$HTTP_POST_VARS['rid']);
+        if ($action == $ESPCONFIG['auto_handler'])
+    		goto_saved("$action?name=$name");
+        else
+            goto_saved($action);
+		return;
+	}
+
 	if(!empty($HTTP_POST_VARS['next'])) {
 		$msg = response_check_required($sid,$HTTP_POST_VARS['sec']);
 		if(empty($msg)) {
+            if ($ESPCONFIG['auth_response'] && auth_get_option('resume'))
+                response_delete($sid, $HTTP_POST_VARS['rid'], $HTTP_POST_VARS['sec']);
 			$HTTP_POST_VARS['rid'] = response_insert($sid,$HTTP_POST_VARS['sec'],$HTTP_POST_VARS['rid']);
 			$HTTP_POST_VARS['sec']++;
 		}
 	}
 	
-	$action = $HTTP_SERVER_VARS['PHP_SELF'];
-	if (!empty($HTTP_SERVER_VARS['QUERY_STRING']))
-		$action .= "?" . $HTTP_SERVER_VARS['QUERY_STRING'];
+	if (!empty($HTTP_POST_VARS['prev']) && $ESPCONFIG['auth_response'] && auth_get_option('navigate')) {
+		if(empty($msg)) {
+            if (auth_get_option('resume'))
+                response_delete($sid, $HTTP_POST_VARS['rid'], $HTTP_POST_VARS['sec']);
+			$HTTP_POST_VARS['rid'] = response_insert($sid,$HTTP_POST_VARS['sec'],$HTTP_POST_VARS['rid']);
+			$HTTP_POST_VARS['sec']--;
+		}
+	}
+    
+    if ($ESPCONFIG['auth_response'] && auth_get_option('resume'))
+        response_import_sec($sid, $HTTP_POST_VARS['rid'], $HTTP_POST_VARS['sec']);
+	
 ?>
-<SCRIPT LANGUAGE="JavaScript">
-<!-- // Begin // This should really go into <HEAD> tag
+<script language="JavaScript">
+<!-- // Begin <?php // This should really go into <head> tag ?>
 
 function other_check(name)
 {
-other = name.split("_");
-
+  other = name.split("_");
   var f = document.phpesp_response;
   for (var i=0; i<=f.elements.length; i++) {
     if (f.elements[i].value == "other_"+other[1]) {
@@ -100,15 +122,26 @@ other = name.split("_");
   }
 }
 // End -->
-</SCRIPT>
+</script>
 <form method="post" name="phpesp_response" action="<?php echo($action); ?>">
 <input type="hidden" name="referer" value="<?php echo($HTTP_POST_VARS['referer']); ?>">
 <input type="hidden" name="userid" value="<?php echo($HTTP_POST_VARS['userid']); ?>">
 <input type="hidden" name="sid" value="<?php echo($sid); ?>">
 <input type="hidden" name="rid" value="<?php echo($HTTP_POST_VARS['rid']); ?>">
 <input type="hidden" name="sec" value="<?php echo($HTTP_POST_VARS['sec']); ?>">
-<?php	render_survey($sid,$HTTP_POST_VARS['sec'],$msg); ?>
-<?php	if($HTTP_POST_VARS['sec'] == $num_sections)	{ ?>
+<?php	survey_render($sid,$HTTP_POST_VARS['sec'],$msg); ?>
+<?php
+		if ($ESPCONFIG['auth_response']) {
+			if (auth_get_option('navigate') && $HTTP_POST_VARS['sec'] > 1) { ?>
+	<input type="submit" name="prev" value="Previous Page">
+<?php
+			}
+			if (auth_get_option('resume')) { ?>
+	<input type="submit" name="resume" value="Save">
+<?php
+			}
+		}
+		if($HTTP_POST_VARS['sec'] == $num_sections)	{ ?>
 	<input type="submit" name="submit" value="Submit Survey">
 <?php	} else { ?>
 	<input type="submit" name="next" value="Next Page">
