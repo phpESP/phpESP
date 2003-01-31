ead3423a5e5e7c21d3cab84df1f734814edd77da
jimmerman <jimmerman@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2003-01-31 04:16:17 +0000

    o Config file clean up.

--- public/handler.php
+++ public/handler.php
@@ -13,9 +13,12 @@
  * See the handler-prefix.php file for details.
  */
 
-	require('/usr/local/lib/php/contrib/phpESP/admin/phpESP.ini.php');
-	require($ESPCONFIG['include_path']."/funcs".$ESPCONFIG['extension']);
-	require($ESPCONFIG['handler_prefix']);
+	if (!defined('ESP_BASE'))
+		define('ESP_BASE', dirname(__FILE__) . '/../');
+
+	require_once(ESP_BASE . '/admin/phpESP.ini.php');
+	require_once($ESPCONFIG['include_path']."/funcs".$ESPCONFIG['extension']);
+	require_once($ESPCONFIG['handler_prefix']);
 	if(!defined('AUTHHAND-OK')) {
 		if (!empty($GLOBALS['errmsg']))
 			echo($GLOBALS['errmsg']);
