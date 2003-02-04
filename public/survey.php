9b4a7e38e11594f091da23c5d3dbcc04d373e786
greggmc <greggmc@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2003-02-04 21:25:11 +0000

    Fixes for auth problems. Fixes for config file changes.

--- public/survey.php
+++ public/survey.php
@@ -7,8 +7,19 @@
 	if (!defined('ESP_BASE'))
 		define('ESP_BASE', dirname(__FILE__) . '/../');
 
-	require_once(ESP_BASE . '/admin/phpESP.ini.php');
-
+	$CONFIG = ESP_BASE . 'admin/phpESP.ini.php';
+	if(!file_exists($CONFIG)) {
+		echo("<b>FATAL: Unable to open config file Aborting.</b>");
+		exit;
+	}
+	if(!extension_loaded('mysql')) {
+		echo('<b>FATAL: Mysql extension not loaded. Aborting.</b>');
+		exit;
+	}
+	require_once($CONFIG);	
+	
+	esp_init_db();	
+	
 	$_name = '';
 	$_title = '';
 	$_css = '';
@@ -20,15 +31,15 @@
 	}
 
 	if (!empty($_name)) {
-        $_sql = "SELECT id,title,theme FROM survey WHERE name = '$_name'";
-        if ($_result = mysql_query($_sql)) {
-            if (mysql_num_rows($_result) > 0)
-                list($sid, $_title, $_css) = mysql_fetch_row($_result);
-            mysql_free_result($_result);
-        }
-        unset($_sql);
-        unset($_result);
-	}
+        	$_sql = "SELECT id,title,theme FROM survey WHERE name = '$_name'";
+        	if ($_result = mysql_query($_sql)) {
+            		if (mysql_num_rows($_result) > 0)
+                		list($sid, $_title, $_css) = mysql_fetch_row($_result);
+            		mysql_free_result($_result);
+        		}
+        	unset($_sql);
+        	unset($_result);
+		}
 
     // call the handler-prefix once $sid is set to handle
     // authentication / authorization
