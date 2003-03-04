d2e572732a6ef3735ed0182c3c2ebbe0d95e45f5
jimmerman <jimmerman@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2003-03-04 23:02:56 +0000

    Redirector to Management Interface.

--- phpESP/index.php
+++ phpESP/index.php
@@ -7,7 +7,7 @@
 
     $url = 
         ((isset($s['HTTPS']) && $s['HTTPS'] == 'on') ? 'https://' : 'http://') .
-        $s['SERVER_NAME'] . dirname($s['SCRIPT_NAME']) . '/manage.php';
+        $s['SERVER_NAME'] . dirname($s['SCRIPT_NAME']) . '/admin/manage.php';
 
     header("Location: $url");
 ?>
