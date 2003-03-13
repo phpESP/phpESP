06148d49906dcd975e1c68587684d3ad2e36a7ea
jimmerman <jimmerman@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2003-03-13 05:49:24 +0000

    Version bump.
    git-svn-id: svn://svn.code.sf.net/p/phpesp/code/trunk@644 b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe

--- phpESP/admin/phpESP.ini.php
+++ phpESP/admin/phpESP.ini.php
@@ -94,7 +94,7 @@ $ESPCONFIG['DEBUG'] = false;
 $ESPCONFIG['name'] = 'phpESP';
 
 // Application version
-$ESPCONFIG['version'] = '1.6';
+$ESPCONFIG['version'] = '1.7-dev';
 
 // Extension of support files
 $ESPCONFIG['extension'] = '.inc';
