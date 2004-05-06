f399de7d95d3d927600b24ed29c5d189dc850f7d
greggmc <greggmc@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2004-05-06 15:56:48 +0000

    Initial commit
    git-svn-id: svn://svn.code.sf.net/p/phpesp/code/trunk@713 b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe

---
+++ phpESP/js/default.js
@@ -0,0 +1,9 @@
+<!--
+function activateConfirm() {
+    if (confirm("Warning! Once activated, this survey can no longer be edited.  Any further changes must be done on a copy.")) {
+	return true;
+    }
+    return false;
+}
+
+-->
