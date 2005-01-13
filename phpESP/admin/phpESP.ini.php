d549c56e74bc6a77ba51b34cd7a8075053192b62
greggmc <greggmc@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2005-01-13 16:42:43 +0000

    bump version number

--- phpESP/admin/phpESP.ini.php
+++ phpESP/admin/phpESP.ini.php
@@ -108,7 +108,7 @@ $ESPCONFIG['DEBUG'] = false;
 $ESPCONFIG['name'] = 'phpESP';
 
 // Application version
-$ESPCONFIG['version'] = '1.7-dev';
+$ESPCONFIG['version'] = '1.7-rc1';
 
 // Extension of support files
 $ESPCONFIG['extension'] = '.inc';
