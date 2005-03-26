b684178402179913fd165f8d78ae25ec9adac2f2
greggmc <greggmc@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2005-03-26 14:35:39 +0000

    update for 1.7.2

--- phpESP/admin/phpESP.ini.php
+++ phpESP/admin/phpESP.ini.php
@@ -108,7 +108,7 @@ $ESPCONFIG['DEBUG'] = false;
 $ESPCONFIG['name'] = 'phpESP';
 
 // Application version
-$ESPCONFIG['version'] = '1.8-dev';
+$ESPCONFIG['version'] = '1.7.2';
 
 // Extension of support files
 $ESPCONFIG['extension'] = '.inc';
