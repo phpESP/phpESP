5051e67664f751cbcb30b88209119b667ba47223
greggmc <greggmc@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2005-04-21 17:12:45 +0000

    moved some text around

--- phpESP/public/handler-prefix.php
+++ phpESP/public/handler-prefix.php
@@ -155,8 +155,7 @@
             return;
 
         if (auth_get_option('resume')) {
-            $HTTP_POST_VARS['rid'] = auth_get_rid($sid, $espuser,
-                    $HTTP_POST_VARS['rid']);
+            $HTTP_POST_VARS['rid'] = auth_get_rid($sid, $espuser, $HTTP_POST_VARS['rid']);
 
             if (!empty($HTTP_POST_VARS['rid']) && (empty($HTTP_POST_VARS['sec']) ||
                     intval($HTTP_POST_VARS['sec']) < 1))
