0f223015020b18969717b4170a721cae9b429aea
greggmc <greggmc@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2005-09-02 19:25:51 +0000

    JS from questions.in moved here

--- js/default.js
+++ js/default.js
@@ -17,4 +17,33 @@ function exportSubmit(type, f) {
     f.where.value=type;
     f.submit();
 }
+
+function clearTextInputs() {
+	var i = 1;
+    while (document.forms[1].elements["choice_content_" + i]) {
+        document.forms[1].elements["choice_content_" + i].value = "";
+        i++;
+    }
+}
+
+function addAnswerLine() {
+    var el = document.getElementById('answerlines');
+    var numchoice = document.getElementById('num_choices').value;
+    numchoice++;
+    var tablerow = el.insertRow(numchoice+1);
+    var tablecell = tablerow.insertCell(-1);
+    tablecell.innerHTML = numchoice+".";
+    tablecell.className = "numbered";
+    tablecell = tablerow.insertCell(-1);
+    var text = "<input type=\"hidden\" name=\"choice_id_"+numchoice+"\" value=\"\" />\n";
+    text = text+"<input type=\"text\" size=\"60\" name=\"choice_content_"+numchoice+"\" value=\"\" />\n";
+    tablecell.innerHTML = text;
+    tablecell.className = "left";
+    document.getElementById('num_choices').value = numchoice;
+}
+    
+    
+function validate() {
+    return true;
+}
 -->
