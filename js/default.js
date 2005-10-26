be8b76874ecfb55dc0dfd29dffd87cd7fbf10c37
greggmc <greggmc@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2005-10-26 20:47:33 +0000

    fixes for XSS and SQL inject from Garvin Hicking <in dot garvin at mail> some css cleanup for 1.8 release

--- js/default.js
+++ js/default.js
@@ -1,14 +1,14 @@
 <!--
 function activateConfirm() {
     if (confirm("Warning! Once activated, this survey can no longer be edited.  Any further changes must be done on a copy.")) {
-	return true;
+        return true;
     }
     return false;
 }
 
 function cancelConfirm() {
     if (confirm("Warning! This survey has not been saved.  Canceling now will remove any changes.")) {
-	return true;
+        return true;
     }
     return false;
 }
@@ -19,7 +19,7 @@ function exportSubmit(type, f) {
 }
 
 function clearTextInputs() {
-	var i = 1;
+    var i = 1;
     while (document.forms[1].elements["choice_content_" + i]) {
         document.forms[1].elements["choice_content_" + i].value = "";
         i++;
@@ -41,9 +41,69 @@ function addAnswerLine() {
     tablecell.className = "left";
     document.getElementById('num_choices').value = numchoice;
 }
-    
-    
+
+
 function validate() {
     return true;
 }
+
+function other_check(name)
+{
+    other = name.split("_");
+    var f = document.phpesp_response;
+    for (var i=0; i<=f.elements.length; i++) {
+        if (f.elements[i].value == "other_"+other[1]) {
+            f.elements[i].checked=true;
+            break;
+        }
+    }
+}
+
+function merge(box) {
+    if(box.options.length >= 2){
+        ml = new Array();
+        for(var i=0; i<box.options.length; i++) {
+            ml[i] = box.options[i].value;
+            sidsArray=ml;
+        }
+        sids = sidsArray.join("+");
+        document.getElementById('sids').value = sids; 
+        form = document.getElementById('merge');
+        form.submit();
+    } else {
+        document.getElementById('error').innerHTML = "<h2>You must select at least two surveys before you can merge</h2>";
+    }
+}
+
+function move(fbox,tbox) {
+    for(var i=0; i<fbox.options.length; i++) {
+        if(fbox.options[i].selected && fbox.options[i].value != "") {
+            var no = new Option();
+            no.value = fbox.options[i].value;
+            no.text = fbox.options[i].text;
+            tbox.options[tbox.options.length] = no;
+            fbox.options[i].value = "";
+            fbox.options[i].text = "";
+        }
+    }
+    BumpUp(fbox);
+}
+
+function BumpUp(box)  {
+    for(var i=0; i<box.options.length; i++) {
+        if(box.options[i].value == "")  {
+            for(var j=i; j<box.options.length-1; j++)  {
+                box.options[j].value = box.options[j+1].value;
+                box.options[j].text = box.options[j+1].text;
+            }
+            var ln = i;
+            break;
+        }
+    }
+    if(ln < box.options.length)  {
+        box.options.length -= 1;
+        BumpUp(box);
+    }
+}
+
 -->
