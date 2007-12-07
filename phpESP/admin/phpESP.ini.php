ffa07f9dc319b6576e04f752c381bcae9406c426
liedekef <liedekef@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2007-12-07 15:15:05 +0000

    some small fixes and some examples on how to integrate phpesp
    git-svn-id: svn://svn.code.sf.net/p/phpesp/code/trunk@946 b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe

--- phpESP/admin/phpESP.ini.php
+++ phpESP/admin/phpESP.ini.php
@@ -150,7 +150,7 @@ $ESPCONFIG['DEBUG'] = false;
 $ESPCONFIG['name'] = 'phpESP';
 
 // Application version
-$ESPCONFIG['version'] = '2.0.0';
+$ESPCONFIG['version'] = '2.0.1';
 
 // Extension of support files
 $ESPCONFIG['extension'] = '.inc';
