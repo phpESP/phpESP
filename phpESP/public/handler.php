d0e8fac48b7e0316b89c68ffeac42fa8929949d3
bishopb <bishopb@b3e5a24c-6043-0410-a8b3-ed4e85eb3ebe>
2009-05-28 01:44:59 +0000

    SFID 2771740

--- phpESP/public/handler.php
+++ phpESP/public/handler.php
@@ -31,6 +31,13 @@
 		return;
 	}
 
+    // did we show feedback on the last page of the survey?
+    // SFID: 2771740
+    if (isset($_REQUEST['feedback']) && is_scalar($feedback = $_REQUEST['feedback']) && 'finished' == $feedback) {
+        // then, the next step is to finish up the survey, since the feedback "interrupted" that processing
+        all_done();
+    }
+
 	// get the survey
 	$sql = "SELECT status, name, public, open_date, close_date FROM ".$GLOBALS['ESPCONFIG']['survey_table']." WHERE id=${sid}";
 	$result = execute_sql($sql);
@@ -136,30 +143,32 @@
 	$msg = '';
 	if(!empty($_REQUEST['submit'])) {
 	    $msg .= response_check_answers($sid,$_SESSION['rid'],$_SESSION['sec']);
+
 	    # we only check the captcha if no all required 
-            if (empty($msg) && $ESPCONFIG['use_captcha']) {
-                require_once(ESP_BASE.'public/captcha_check.php');
-                $msg .= response_check_captcha("captcha_check",1);
-            }   
+        if (empty($msg) && $ESPCONFIG['use_captcha']) {
+            require_once(ESP_BASE.'public/captcha_check.php');
+            $msg .= response_check_captcha("captcha_check",1);
+        }   
 
-            if (empty($msg)) {
-                if ($ESPCONFIG['auth_response'] && auth_get_option('resume')) {
-                    // submitting a previously saved survey
-                    esp_require_once('/lib/espsurveystat');
-                    survey_stat_decrement(SURVEY_STAT_SUSPENDED, $sid);
+        if (empty($msg)) {
+            if ($ESPCONFIG['auth_response'] && auth_get_option('resume')) {
+                // submitting a previously saved survey
+                esp_require_once('/lib/espsurveystat');
+                survey_stat_decrement(SURVEY_STAT_SUSPENDED, $sid);
 
-                    // delete the previous responses
-                    response_delete($sid, $_SESSION['rid'], $_SESSION['sec']);
-                }
-		$_SESSION['rid'] = response_insert($sid,$_SESSION['sec'],$_SESSION['rid']);
-		response_commit($_SESSION['rid']);
-		response_send_email($sid,$_SESSION['rid']);
-		$_SESSION['rid']="";
-		$_SESSION['sec']="";
-			
-		goto_thankyou($sid,$_REQUEST['referer']);
-		return;
-	    }
+                // delete the previous responses
+                response_delete($sid, $_SESSION['rid'], $_SESSION['sec']);
+            }
+
+            $_SESSION['rid'] = response_insert($sid,$_SESSION['sec'],$_SESSION['rid']);
+
+            // paint the feedback
+            // NOTE: This function may exit
+            paint_feedback_end_of_survey($sid, $_SESSION['rid'], $_SESSION['sec']);
+
+            // no feedback, so jump to thank you
+            all_done();
+        }
 	}
 
     // if we encounter the variable resume in the $_REQUEST
@@ -177,10 +186,14 @@
 	if(!empty($_REQUEST['next'])) {
 		$msg = response_check_answers($sid,$_SESSION['rid'],$_SESSION['sec']);
 		if(empty($msg)) {
-            		if ($ESPCONFIG['auth_response'] && auth_get_option('resume'))
-                		response_delete($sid, $_SESSION['rid'], $_SESSION['sec']);
-            		$_SESSION['rid'] = response_insert($sid,$_SESSION['sec'],$_SESSION['rid']);
-			$_SESSION['sec']++;
+            // record the response
+            if ($ESPCONFIG['auth_response'] && auth_get_option('resume'))
+                response_delete($sid, $_SESSION['rid'], $_SESSION['sec']);
+            $_SESSION['rid'] = response_insert($sid,$_SESSION['sec'],$_SESSION['rid']);
+
+            // show the feedback
+            // NOTE: This method exits
+            paint_feedback_end_of_section($sid, $_SESSION['rid'], $_SESSION['sec']);
 		}
 	}
 	
@@ -208,40 +221,237 @@
     }
 	
 ?>
-<form method="post" id="phpesp_response" action="<?php echo($action); ?>">
-    <fieldset class="hidden">
-<input type="hidden" name="referer" value="<?php echo ($_REQUEST['referer']); ?>" />
-<input type="hidden" name="direct" value="<?php echo($_REQUEST['direct']); ?>" />
-<input type="hidden" name="userid" value="<?php echo($_REQUEST['userid']); ?>" />
-<input type="hidden" name="sid" value="<?php echo($sid); ?>" />
-<input type="hidden" name="rid" value="<?php echo($_SESSION['rid']); ?>" />
-<input type="hidden" name="sec" value="<?php echo($_SESSION['sec']); ?>" />
-<input type="hidden" name="name" value="<?php echo($name); ?>" />
-    </fieldset>
 <?php
-        survey_render($sid,$_SESSION['sec'],$_SESSION['rid'],$msg);
-?>
-    <fieldset>
-<?php
-		if ($ESPCONFIG['auth_response']) {
-			if (auth_get_option('navigate') && $_SESSION['sec'] > 1) {
-                echo(mksubmit("prev", _('Previous Page')));
-			}
-			if (auth_get_option('resume')) {
-                echo(mksubmit("resume", _('Save')));
-			}
-		}
-		if($_SESSION['sec'] == $num_sections) {
-            if ($ESPCONFIG['use_captcha']) {
-                print '<table><tr><td><img src="'.$ESPCONFIG['base_url'].'public/captcha.php"></td>';
-                print '<td>';
-                echo _("Please fill in the code displayed here.");
-                print '<br><input type="text" name="captcha_check"></td></tr></table>';
-            }
-            echo(mksubmit("submit", _('Submit Survey')));
-        } else {
-            echo(mksubmit("next", _('Next Page')));
+    paint_submission_form_open();
+    survey_render($sid,$_SESSION['sec'],$_SESSION['rid'],$msg);
+    echo '<fieldset>';
+    if ($ESPCONFIG['auth_response']) {
+        if (auth_get_option('navigate') && $_SESSION['sec'] > 1) {
+            echo(mksubmit("prev", _('Previous Page')));
         }
+        if (auth_get_option('resume')) {
+            echo(mksubmit("resume", _('Save')));
+        }
+    }
+    if($_SESSION['sec'] == $num_sections) {
+        if ($ESPCONFIG['use_captcha']) {
+            print '<table><tr><td><img src="'.$ESPCONFIG['base_url'].'public/captcha.php"></td>';
+            print '<td>';
+            echo _("Please fill in the code displayed here.");
+            print '<br><input type="text" name="captcha_check"></td></tr></table>';
+        }
+        echo(mksubmit("submit", _('Submit Survey')));
+    } else {
+        echo(mksubmit("next", _('Next Page')));
+    }
+    echo '</fieldset>';
+    paint_submission_form_close();
 ?>
-        </fieldset>
+<?php
+/**
+Functions to display feedback, if any, on the selected responses.
+See SFID 2771740
+*/
+function paint_feedback_end_of_survey($sid, $rid, $sec) {
+    // paint the feedback
+    // NOTE: if there is any, this function exits
+    paint_feedback($sid, $rid, $sec, array ('feedback' => 'finished'));
+}
+
+function paint_feedback_end_of_section($sid, $rid, $sec) {
+    // increment the section
+    $_SESSION['sec']++;
+
+    // paint my feedback
+    paint_feedback($sid, $rid, $sec);
+}
+
+function paint_feedback($sid, $rid, $sec, $additional = array ()) {
+    // get all the feedback so far, if any
+    $hasFeedback = get_feedback($feedback, $sid, $rid, $sec);
+
+    // if there are is feedback, paint it and exit
+    if ($hasFeedback) {
+        // open teh form
+        paint_submission_form_open($additional);
+
+        // ... paint feedback to each response
+        echo '<table>';
+        array_walk($feedback, 'paint_feedback_row');
+        echo '</table>';
+
+        // paint the next button
+        // NOTE: don't call it "next", because the logic above has specific expectations about
+        // NOTE: the cases when that button is pressed
+        echo '<fieldset>';
+        echo mksubmit("go", _('Next Page'));
+        echo '</fieldset>';
+
+        // close the form
+        paint_submission_form_close();
+
+        // all done
+        exit;
+    }
+}
+
+/**
+Returns (via PBR first parameter) the responses for the given survey ($sid), identified by the given response ID ($rid),
+in the indicated section ($sec).  The format of $responses is:
+array (
+   // 'question number' => array ('Question?', array ('Choice1', 'Feedback1'), ..., array ('ChoiceN', 'FeedbackN'));
+);
+*/
+function get_feedback(&$responses, $sid, $rid, $sec) {
+    // get the questions for each section (by ID)
+    // NOTE: we're given section 1-base, but this method has section 0-base
+    $sec -= 1;
+    $section_questions = survey_get_section_questions($sid);
+    if (! array_key_exists($sec, $section_questions)) {
+        // invalid section, can't possibly be any feedback
+        return;
+    }
+
+    // figure out on what question number we start
+    $qnum = 0;
+    for ($i = 0; $i <= $sec; $i++) {
+        $qnum += count($section_questions[$i]);
+    }
+
+    // get the responses for just this section
+    $allResponses = response_select($sid, $rid, 'content', $section_questions[$sec]);
+
+    // convert any rank or !other to multi-response format
+    foreach ($allResponses as $qid => $feedback) {
+        // if this is an other
+        if (false !== strpos($qid, '_')) {
+            // pull it out
+            unset($allResponses[$qid]);
+
+            /*
+            // store it as multi
+            list ($qid,$sub) = explode('_', $qid);
+            $allResponses[$qid][] = $feedback;
+            */
+        }
+    }
+
+    // put them into our desired format
+    $hasFeedback = false;
+    foreach ($allResponses as $qid => $feedback) {
+        // figure out if this is a multi-response
+        $hasMulti = (is_array($feedback[0]) ? true : false);
+
+        // initialize the structure with the question name
+        $responses[$qnum] = array ($hasMulti ? $feedback[0][0] : $feedback[0]);
+
+        // if this is a multiple response, add them each in
+        if ($hasMulti) {
+            foreach ($feedback as $response) {
+                $responses[$qnum][] = array ($response[1], $response[3]);
+                if (! empty($response[3])) {
+                    $hasFeedback = true;
+                }
+            }
+
+        // otherwise, add this one in
+        } else {
+            $responses[$qnum][] = array ($feedback[1], $feedback[3]);
+            if (! empty($feedback[3])) {
+                $hasFeedback = true;
+            }
+        }
+
+        // increment the question number
+        $qnum++;
+    }
+
+    return $hasFeedback;
+}
+
+function paint_feedback_row($response, $number) {
+    // break apart the feedback data structure
+    $question = array_shift($response);
+
+    // initialize variables we'll use in our output
+    $label1 = _('Your choice:');
+    $label2 = _('Feedback:');
+
+    // output the question
+    echo <<<EOHTML
+<tr>
+  <td style='padding-top: .5em; text-align: right;'>{$number}.</td>
+  <td style='padding-top: .5em;'>{$question}</td>
+</tr>
+EOHTML;
+
+    // output each choice and feedback
+    foreach ($response as $info) {
+        list ($choice, $feedback) = $info;
+
+        if (empty($feedback)) {
+            $feedback = _('None');
+        }
+        echo <<<EOHTML
+<tr>
+  <td style='text-align: right;'>{$label1}</td>
+  <td>{$choice}</td>
+</tr>
+<tr>
+  <td style='text-align: right;'>{$label2}</td>
+  <td>{$feedback}</td>
+</tr>
+EOHTML;
+    }
+}
+
+/**
+Refactored methods.
+*/
+function paint_submission_form_open($additional = array ()) {
+    global $action, $sid, $name;
+    echo <<<EOHTML
+<form method="post" id="phpesp_response" action="$action">
+<fieldset class="hidden">
+<input type="hidden" name="referer" value="{$_REQUEST['referer']}" />
+<input type="hidden" name="direct"  value="{$_REQUEST['direct']}" />
+<input type="hidden" name="userid"  value="{$_REQUEST['userid']}" />
+<input type="hidden" name="sid"     value="{$sid}" />
+<input type="hidden" name="rid"     value="{$_SESSION['rid']}" />
+<input type="hidden" name="sec"     value="{$_SESSION['sec']}" />
+<input type="hidden" name="name"    value="{$name}" />
+EOHTML;
+
+    foreach ($additional as $field => $value) {
+        echo "<input type='hidden' name='$field' value='$value' />";
+    }
+
+    echo <<<EOHTML
+</fieldset>
+EOHTML;
+}
+
+function paint_submission_form_close() {
+    echo <<<EOHTML
 </form>
+EOHTML;
+}
+
+function all_done() {
+    global $sid;
+
+    // commit the response and send an email
+    response_commit($_SESSION['rid']);
+    response_send_email($sid,$_SESSION['rid']);
+
+    // initialize the state variables
+    $_SESSION['rid']="";
+    $_SESSION['sec']="";
+
+    // go to the thank you
+    goto_thankyou($sid, $_REQUEST['referer']);
+    exit;
+}
+
+
+?>
